0219a9e44d025dd2a9156db554338b89ae10710e
SickBoy-Droid <hitechgenius123@gmail.com>
2020-04-19 18:09:29 +0530

    add ads

--- app/src/main/java/com/gameofcoding/adstest/activities/MainActivity.java
+++ app/src/main/java/com/gameofcoding/adstest/activities/MainActivity.java
@@ -16,7 +16,7 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-	Adview adview = findViewById(R.id.adView1);
+	AdView adview = findViewById(R.id.adView1);
 	MobileAds.initialize(this, new OnInitializationCompleteListener() {
             @Override
             public void onInitializationComplete(InitializationStatus initializationStatus) {

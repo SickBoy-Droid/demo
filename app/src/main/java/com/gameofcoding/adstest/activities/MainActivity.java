795931a14d53b4eb1ac634fbae4dbcd44009f6b1
SickBoy-Droid <hitechgenius123@gmail.com>
2020-04-19 18:00:32 +0530

    add ads

---
+++ app/src/main/java/com/gameofcoding/adstest/activities/MainActivity.java
@@ -0,0 +1,29 @@
+package com.gameofcoding.adstest.activities;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import com.google.android.gms.ads.MobileAds;
+import com.google.android.gms.ads.initialization.InitializationStatus;
+import com.google.android.gms.ads.initialization.OnInitializationCompleteListener;
+import com.google.android.gms.ads.AdRequest;
+import com.google.android.gms.ads.AdView;
+
+import com.gameofcoding.adstest.R;
+
+public class MainActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+	Adview adview = findViewById(R.id.adView1);
+	MobileAds.initialize(this, new OnInitializationCompleteListener() {
+            @Override
+            public void onInitializationComplete(InitializationStatus initializationStatus) {
+            }
+        });
+	AdRequest adRequest = new AdRequest.Builder().build();
+        adView.loadAd(adRequest);
+	
+    }
+}
---
+++ app/src/main/java/com/gameofcoding/adstest/utils/AppConstants.java
@@ -0,0 +1,6 @@
+package com.gameofcoding.adstest.utils;
+
+public abstract class AppConstants {
+	public static abstract class PreferenceConstants {
+	}
+}
